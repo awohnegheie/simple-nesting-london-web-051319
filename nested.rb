3eca9f1083df38e7940c928f6f6c6ce5cb8d3067
Awoh Negheie <awohnegheie@gmail.com>
2019-05-10 01:28:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {
+	:known_for => "Ruby",
+	:languages => ["Lisp", "C"]
+	}
+	programmer_hash
 
 end
 
